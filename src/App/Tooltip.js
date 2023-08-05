3a9042df11f11aac49dc305eddf40c7d1e7a5a88
narendravyas17 <141482554+narendravyas17@users.noreply.github.com>
2023-08-05 14:25:32 +0530

    added code

---
+++ src/App/Tooltip.js
@@ -0,0 +1,47 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable linebreak-style */
+import Cmp, { doSomething } from './Component';
+
+console.log('Tooltip running');
+
+export class Tooltip extends Cmp {
+  constructor(closeNotifierFunction, text, hostElementId) {
+    super(hostElementId);
+    this.closeNotifier = closeNotifierFunction;
+    this.text = text;
+    this.create();
+  }
+
+  closeTooltip = () => {
+    this.detach();
+    this.closeNotifier();
+  };
+
+  create() {
+    const tooltipElement = document.createElement('div');
+    tooltipElement.className = 'card';
+    const tooltipTemplate = document.getElementById('tooltip');
+    const tooltipBody = document.importNode(tooltipTemplate.content, true);
+    tooltipBody.querySelector('p').textContent = this.text;
+    tooltipElement.append(tooltipBody);
+
+    const hostElPosLeft = this.hostElement.offsetLeft;
+    const hostElPosTop = this.hostElement.offsetTop;
+    const hostElHeight = this.hostElement.clientHeight;
+    const parentElementScrolling = this.hostElement.parentElement.scrollTop;
+
+    const x = hostElPosLeft + 20;
+    const y = hostElPosTop + hostElHeight - parentElementScrolling - 10;
+
+    tooltipElement.style.position = 'absolute';
+    tooltipElement.style.left = `${x}px`; // 500px
+    tooltipElement.style.top = `${y}px`;
+
+    tooltipElement.addEventListener('click', this.closeTooltip);
+    this.element = tooltipElement;
+  }
+}
---
+++ src/Utility/Analytics.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const intervalId = setInterval(() => {
+  console.log('Sending analytics data...');
+}, 2000);
+
+document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+  clearInterval(intervalId);
+});
